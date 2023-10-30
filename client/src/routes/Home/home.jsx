e49d165d3f97099f5ecb1c3b8e45a987dd50bbff
haris <you@example.com>
2023-10-30 12:35:12 +0530

    library

---
+++ client/src/routes/Home/home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div>
+<h1>Home</h1>
+    <p>Minature Vite + React website.</p>
+    <p>It uses NodeJs, Express & MongoDB as a Background!</p>
+
+    </div>
+  )
+}
+
+export default Home;
